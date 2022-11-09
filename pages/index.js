4b72f6fd07ffe4c758fdac21a3c8e8b346ecedff
Diego Carvalho <fdiegocl@gmail.com>
2022-11-09 11:47:35 -0300

    Desafios

--- pages/index.js
+++ pages/index.js
@@ -5,56 +5,76 @@ import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-    const estilosDaHomePage = { 
-        // backgroundColor: "red"
-    };
+    
+    const estilosDaHomePage = {
 
-    // console.log(config.playlists);
+    };
 
     return (
         <>
             <CSSReset />
             <div style={estilosDaHomePage}>
-            <Menu></Menu>
-            <Header></Header>
-            <Timeline playlists={config.playlists}>
-                Conteudo
-            </Timeline>            
+                <Menu></Menu>
+                <Banner></Banner>
+                <Header></Header>
+                <Timeline playlists={config.playlists}>
+                    {/* Conteudo */}
+                </Timeline>
+                <Favorites favorites={config.favorites}>
+                    {/* Conteudo */}
+                </Favorites>
             </div>
         </>
     )
-  }
-  
-  export default HomePage
+}
 
-//   function Menu() {
-//     return (
-//         <div>
-//             Menu
-//         </div>
-//     )
-//   }
+export default HomePage
 
-  const StyledHeader = styled.div`
-    img {
-        margin-top: 50px;
-        width: 80px;
-        height: 80px;
-        border-radius: 50%;
-    }
-    .user-info {
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding:  16px 32px;
-        gap: 16px;
-    }
-  `;
-  function Header() {
+const StyledBanner = styled.div`
+
+        section {
+            height: 230px;
+            background-image: url("https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+    `
+
+function Banner() {    
+
+    return (
+
+        <StyledBanner>
+            <section></section>
+        </StyledBanner>
+
+    )
+
+}
+
+const StyledHeader = styled.div`
+
+        img {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+        }
+        .user-info {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            padding:  16px 32px;
+            gap: 16px;
+        }
+        
+    `;
+
+function Header() {    
+
     return (
         <StyledHeader>
-            {/* <img src="banner" /> */}
-
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
                 <div>
@@ -64,13 +84,14 @@ function HomePage() {
             </section>
         </StyledHeader>
     )
-  }
+}
 
-  function Timeline(props) {
-    // console.log("Dentro do Componente", props.playlists)
+function Timeline(props) {
+    
     const playListNames = Object.keys(props.playlists)
     // Statement
     // Retorno por expressao
+
     return (
         <StyledTimeline>
             {playListNames.map((playListName) => {
@@ -96,5 +117,62 @@ function HomePage() {
                 )
             })}
         </StyledTimeline>
+    )    
+}
+
+const StyledFavors = styled.div`
+
+        flex: 1;
+        width: 100%;
+        padding: 10px 32px 10px 32px;
+        overflow: hidden;
+
+        h2 {
+            font-size: 16px;
+            margin-bottom: 16px;
+            text-transform: capitalize;
+        }
+        section {
+            display: flex;
+        }
+        img {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+        }
+        .favor-info {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 120px;
+            padding: 16px 16px;
+        }
+        
+    `;
+
+function Favorites(props) {    
+
+    const favoriteNames = Object.keys(props.favorites)
+    console.log(config.favorites)
+
+    return(
+        <StyledFavors>            
+            <h2>Favoritos</h2>
+            <section>
+                {                
+                    favoriteNames.map((favoriteName) => {
+                        const favors = props.favorites[favoriteName];
+                        return (                        
+                            <section className="favor-info">
+                                <div>
+                                    <img src={favors.perfil_image} />
+                                </div>
+                                <p>{favors.name}</p>
+                            </section>
+                        )                    
+                    })
+                }
+            </section>
+        </StyledFavors>
     )
-  }
+}
